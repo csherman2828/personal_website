caa28a6ecbf0951c44a8146f84aca4a0fcee523a
csherman2828 <csherman2828@gmail.com>
2021-02-05 18:16:28 -0600

    pages for router

---
+++ src/pages/CS331.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>CS331 Page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CS331',
+}
+</script>
---
+++ src/pages/CS351.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h2>CS351 Page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CS351',
+}
+</script>
