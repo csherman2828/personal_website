bc8081be5f832209f796cb159f555c21f2188af0
csherman2828 <csherman2828@gmail.com>
2021-02-05 18:14:45 -0600

    add common header for pages

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="siteHeader">
+    <h1>Christopher Sherman's Website</h1>
+    <p>
+      Undergraduate Teaching Assistant for the Dept. of Computer Science,
+      Illinois Institute of Technology
+    </p>
+    <p> Email: csherman1@hawk.iit.edu</p>
+    <a @click="go('CS331')">CS331</a> | <a @click="go('CS351')">CS351</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'Header',
+  methods: {
+    go(routeName) {
+      if(this.$route.name != routeName)
+      this.$router.push({name: routeName});
+    }
+  }
+}
+</script>
+
+<style>
+
+#siteHeader {
+  border-bottom: 5px solid black;
+}
+
+</style>
